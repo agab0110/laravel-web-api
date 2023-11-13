584903e595dc244176696404e13857f53ae033f8
agab0110 <agabrielex01@gmail.com>
2023-11-13 12:36:53 +0100

    updated data visualize

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -14,6 +14,9 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $data = parent::toArray($request);
+        $data['status'] = $this->completed ? 'finished' : 'open';
+
+        return $data;
     }
 }
