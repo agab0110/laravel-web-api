dd66448de4161dc4136d81abf42e9b595ffd9895
agab0110 <agabrielex01@gmail.com>
2023-11-13 14:33:36 +0100

    updated rules

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -22,7 +22,8 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'sometimes|required|string|max:255'
+            'title' => 'sometimes|required|string|max:255',
+            'completed' => 'sometimes|required|boolean'
         ];
     }
 }
