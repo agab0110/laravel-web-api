21b27329074a5804df60a909e218a009965eeaff
agab0110 <agabrielex01@gmail.com>
2023-11-13 16:10:56 +0100

    updated migration adding a foreign key

--- database/migrations/2023_11_13_110133_create_tasks_table.php
+++ database/migrations/2023_11_13_110133_create_tasks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->boolean('completed')->default(false);
+            $table->foreignId('creator_id')->constrained('users');      // chiave esterna su users
             $table->timestamps();
         });
     }
