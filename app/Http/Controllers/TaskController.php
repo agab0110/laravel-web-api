709338871300acb6c0efe11b6d9c0288e44af0a2
agab0110 <agabrielex01@gmail.com>
2023-11-13 12:06:17 +0100

    added index function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index() {
+        return response()->json(Task::all());
+    }
 }
