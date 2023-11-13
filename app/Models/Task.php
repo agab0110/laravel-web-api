d3b3f44139859c4051ad4dfc8cf097996ee7413c
agab0110 <agabrielex01@gmail.com>
2023-11-13 12:37:09 +0100

    added casts and hidden propreties

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $hidden = [
+        'updated_at',
+        'created_at'
+    ];
+
+    protected $casts = [
+        'completed' => 'boolean'
+    ];
 }
