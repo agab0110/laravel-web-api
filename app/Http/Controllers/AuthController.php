4e792c3e381a91739a70cf3061a8b09fd6cf5ea4
agab0110 <agabrielex01@gmail.com>
2023-11-15 10:09:06 +0100

    added register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
@@ -15,7 +16,7 @@ class AuthController extends Controller
         if (!Auth::attempt($validated)) {       // Auth è una funzione di laravel per l'autenticazione
             return response()->json([
                 'message' => 'Invalid login information',   // se il tentativo non va a buon fine allora torniamo un 404, altrimenti
-            ], 404);
+            ], 401);
         }
 
         $user = User::where('email', $validated['email'])->first();     // cerchiamo l'utente nel database
@@ -24,5 +25,17 @@ class AuthController extends Controller
             'access_token' => $user->createToken('api_token')->plainTextToken,      // ritorniamo il token di accesso dell'utente come testo
             'token_type' => 'Bearer'    // Bearer è il tipo di token
         ]);
+    }
+
+    public function register(RegisterRequest $request) {
+        $validated = $request->validated();
+
+        $user = User::create($validated);
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $user->createToken('api_token')->plainTextToken,
+            'token_type' => 'Bearer'
+        ], 201);
     }
 }
