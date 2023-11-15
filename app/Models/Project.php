66f4a5bb533607538642168af1fad078f8bd7574
agab0110 <agabrielex01@gmail.com>
2023-11-15 11:24:06 +0100

    added foreign key to user

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
@@ -14,7 +15,11 @@ class Project extends Model
         'title',
     ];
 
-    public function tasks(): HasMany {      // grazie alla naming convention possiamo omettere la colonna a cui si riferisce    
+    public function tasks(): HasMany {      // grazie alla naming convention possiamo omettere la colonna a cui si riferisce
         return $this->hasMany(Task::class);
+    }
+
+    public function creator(): BelongsTo {
+        return $this->belongsTo(User::class, 'creator_id');
     }
 }
