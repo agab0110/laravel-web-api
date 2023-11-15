2a5bce33d1acbbb096a0552e5d0df879cbad2309
agab0110 <agabrielex01@gmail.com>
2023-11-15 11:06:37 +0100

    added foreign key to project

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -30,6 +30,10 @@ class Task extends Model
         return $this->belongsTo(User::class, 'creator_id');
     }
 
+    public function project(): BelongsTo {
+        return $this->belongsTo(Project::class);
+    }
+
     protected static function booted(): void {      // metodo globale per far si che si vedano solo le tasks dell'utente loggato
         static::addGlobalScope('creator', function (Builder $builder) {
             $builder->where('creator_id', Auth::id());      // non so perché da errore ma funziona
