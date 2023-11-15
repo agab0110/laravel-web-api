57d7311450df568498e02ba2c31ae7662cb34875
agab0110 <agabrielex01@gmail.com>
2023-11-15 11:22:39 +0100

    updated project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+    ];
+
+    public function tasks(): HasMany {      // grazie alla naming convention possiamo omettere la colonna a cui si riferisce    
+        return $this->hasMany(Task::class);
+    }
 }
