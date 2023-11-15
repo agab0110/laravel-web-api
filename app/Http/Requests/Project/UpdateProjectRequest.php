f5ac32f7a27b2d151d9d55776a38dad815131c39
agab0110 <agabrielex01@gmail.com>
2023-11-15 12:34:39 +0100

    added rules

--- app/Http/Requests/Project/UpdateProjectRequest.php
+++ app/Http/Requests/Project/UpdateProjectRequest.php
@@ -11,7 +11,7 @@ class UpdateProjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'sometimes|required|string|max:255',     // il campo, se presente, non dovrà essere null
         ];
     }
 }
